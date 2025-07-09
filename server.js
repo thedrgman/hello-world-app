ea1a5345314f0675aaf220a21f2af04291ce29b4
David Gameiro <80590783+thedrgman@users.noreply.github.com>
2025-07-09 11:18:26 -0400

    Update server.js

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+  });
+}
 
-module.exports = app; // Export for testing
+module.exports = app;
