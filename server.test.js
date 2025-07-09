e816278094e82b389cad5c07e00f7e4c52c5dfd9
David Gameiro <80590783+thedrgman@users.noreply.github.com>
2025-07-09 11:43:48 -0400

    Update server.test.js

--- server.test.js
+++ server.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const app = require('./server.js');
 
 describe('GET /', () => {
-  it('should return "Hello World"', async () => {
+  it('should return "Hello World, David"', async () => {
     const response = await request(app).get('/');
     expect(response.status).toBe(200);
     expect(response.text).toBe('Hello World');
